ebf1f557d88eed70012bc6cfec969ee95616807b
Lmmhhhh <lmh10087215@gmail.com>
2025-07-14 16:51:33 +0900

    chore: 기본 코드 준비

---
+++ src/main/java/gift/controller/ProductController.java
@@ -0,0 +1,64 @@
+package gift.controller;
+
+
+import gift.dto.request.ProductRequest;
+import gift.dto.request.ProductUpdateRequest;
+import gift.dto.response.ProductResponse;
+import gift.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> register(@RequestBody @Valid ProductRequest request) {
+        ProductResponse response = productService.register(request);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.id())
+                .toUri();
+        return ResponseEntity.created(location).body(response);
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<ProductResponse> getProduct(@PathVariable Long productId) {
+        return ResponseEntity.ok(productService.getProduct(productId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @PatchMapping("/{productId}")
+    public ResponseEntity<ProductResponse> updateProduct(@PathVariable Long productId, @RequestBody @Valid ProductUpdateRequest request){
+        ProductResponse response = productService.updateProduct(productId,request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long productId) {
+        productService.deleteProduct(productId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/search")
+    public List<ProductResponse> searchByName(@RequestParam String name) {
+        return productService.searchByName(name);
+    }
+}
---
+++ src/main/java/gift/repository/MemberRepositoryImpl.java
@@ -0,0 +1,49 @@
+package gift.repository;
+
+import gift.domain.Member;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class MemberRepositoryImpl implements MemberRepository {
+
+    private final JdbcClient jdbcClient;
+
+    public MemberRepositoryImpl(JdbcClient jdbcClient){
+        this.jdbcClient = jdbcClient;
+    }
+
+    @Override
+    public Member register(Member member){
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcClient.sql("insert into members (email, pwd) values (:email, :pwd)")
+                .param("email", member.getEmail())
+                .param("pwd", member.getPwd())
+                .update(keyHolder);
+
+        Long id = keyHolder.getKey().longValue();
+
+        return new Member(id, member.getEmail(), member.getPwd());
+    }
+
+    @Override
+    public Optional<Member> findByEmail(String email){
+        return jdbcClient.sql("select * from members where email = :email")
+                .param("email", email)
+                .query(Member.class)
+                .optional();
+    }
+
+    @Override
+    public Optional<Member> findById(Long id){
+        return jdbcClient.sql("select * from members where id = :id")
+                .param("id",id)
+                .query(Member.class)
+                .optional();
+    }
+}
